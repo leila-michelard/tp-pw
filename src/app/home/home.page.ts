433a0b7eaa33684c2b415c5bd1f784b873826be4
Leila Michelard <leila.michelard@gmail.com>
2019-03-13 16:10:51 +0100

    IONIC compris

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-
+import {Movie} from '../details/movie';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
+  movies: Movie[] = [{title: 'Titre1'}, {title: 'Titre2'}, {title: 'Titre3'}];
 }
