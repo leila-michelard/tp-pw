7d72f60f64bf281ceb2f05357ebf051a5507e372
Leila Michelard <leila.michelard@gmail.com>
2019-03-27 14:32:01 +0100

    FIN TP1 (sauf BOUTON RANDOM)

--- src/app/details/details.page.ts
+++ src/app/details/details.page.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import {Movie} from './movie';
 
 @Component({
   selector: 'app-details',
   templateUrl: './details.page.html',
   styleUrls: ['./details.page.scss'],
 })
-export class DetailsPage implements OnInit {
+export class DetailsPage {
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private readonly router: Router) {
+    console.log(this.router.getCurrentNavigation().extras.state);
   }
 
 }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import {Movie} from '../details/movie';
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  movies: Movie[] = [{title: 'Titre1'}, {title: 'Titre2'}, {title: 'Titre3'}];
+  movies: Movie[] = [];
+
+  constructor(private readonly router: Router) {}
+
+  getMovies(titre: string): void {
+    console.log(titre);
+    if (titre.length >= 3) {
+      this.movies = [
+        {title : 'Movie1'},
+        {title : 'Movie2'}
+      ];
+    } else {
+      this.movies = [];
+    }
+  }
+
+  showDetails(m: Movie): void {
+    this.router.navigate(['/details'], {state: m});
+
+  }
 }
